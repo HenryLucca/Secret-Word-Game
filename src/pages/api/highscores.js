3ff1e65af2ac06af4c09d80fea2e0f8bdcc1871d
Henry Lucca <henryluccag@gmail.com>
2023-05-24 17:39:10 -0300

    feat: highscore api

--- src/pages/api/highscores.js
+++ src/pages/api/highscores.js
@@ -1,3 +1,31 @@
+import { connectToDatabase } from "@/src/mongodb/mongodb"
+import { NextApiRequest, NextApiResponse } from "next";
+
 export default async function handler(req, res) {
-    
+    try{
+        const { method } = req;
+        const { name, score } = req.body;
+
+        if (method === 'GET'){
+            const db = await connectToDatabase();
+            const collection = db.collection("highscores");
+            const highscores = await collection.find({}).toArray();
+            res.status(200).json(highscores);
+        } else if (method === 'POST'){
+            const db = await connectToDatabase();
+            const collection = db.collection("highscores");
+            const highscores = await collection.insertOne({name, score});
+            res.status(200).json(highscores);
+        } else if (method === 'DELETE'){
+            const { id } = req.body;
+            const db = await connectToDatabase();
+            const collection = db.collection("highscores");
+            collection.deleteOne(id);
+        }
+            
+    }
+    catch(err){
+        res.status(500).json({error: err.message});
+    }
+
 }
